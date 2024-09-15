d6b244e468b76facd536a3884ef8ef55dca6d8c8
SrCrow02 <seuemail@example.com>
2024-09-15 14:04:53 -0300

    Now i adjust the projects section and contact and footer

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import marioImage from '../assets/mario.jpeg'; // Use sua própria imagem para projetos
-
+import marioImage from '../assets/mario.jpeg'; 
+import blogImage from '../assets/blog.jpeg';
+import galeryImage from '../assets/galery.png';
+import { FaHtml5, FaGithub, FaReact, FaNodeJs, FaCss3Alt, FaJsSquare, FaDiscord,  FaSass } from 'react-icons/fa'; // Importando ícones das tecnologias
+import { SiPrisma, SiTypescript, SiPostgresql, SiHandlebarsdotjs } from 'react-icons/si';
 const Main = () => {
   return (
     <section className="flex flex-col">
@@ -30,48 +33,103 @@ const Main = () => {
     <h2 className="text-4xl font-bold mb-6 text-blue-600 text-center">Projects</h2>
     
     <div className="flex flex-col space-y-6 w-full max-w-6xl mx-auto">
+        {/* Blog Project */}
         <div className="bg-white p-8 rounded-lg shadow-lg border-t-4 border-blue-600 flex flex-col md:flex-row-reverse items-center">
         <div className="w-full md:w-1/2 flex justify-center mb-6 md:mb-0">
-            <img src={marioImage} alt="Blog Project" className="w-full max-w-xl h-auto object-cover rounded-lg" />
+            <img src={blogImage} alt="Blog Project" className="w-full max-w-xl h-auto object-cover rounded-lg" />
         </div>
         <div className="w-full md:w-1/2 text-center md:text-left px-6">
             <h3 className="text-2xl font-semibold mb-2">Blog</h3>
             <p className="mb-4">I did this project just for fun, it actually works, but is not amazing</p>
             <div className="flex space-x-4 justify-center md:justify-start">
-            <button className="bg-blue-500 text-white px-6 py-3 rounded-full hover:bg-blue-600 transition duration-300">See Live</button>
-            <button className="bg-gray-500 text-white px-6 py-3 rounded-full hover:bg-gray-600 transition duration-300">Source Code</button>
+            {/* <button className="bg-blue-500 text-white px-6 py-3 rounded-full hover:bg-blue-600 transition duration-300">See Live</button> */}
+            <a href="https://github.com/SrCrow02/DevBlog" target="_blank" rel="noopener noreferrer">
+              <button className="bg-gray-500 text-white px-6 py-3 rounded-full hover:bg-gray-600 transition duration-300">
+                Source Code
+              </button>
+            </a>
+            </div>
+            <div className="flex space-x-4 mt-4">
+              <FaNodeJs className="text-green-500" size={30} />
+              <FaCss3Alt className="text-blue-700" size={30} />
+              <FaHtml5 className="text-blue-700" size={30} />
+              <SiPrisma className="text-blue-700" size={30} />
+              <SiTypescript className="text-blue-700" size={30} />
+              <SiPostgresql className="text-blue-700" size={30} />
+              <SiHandlebarsdotjs className="text-blue-700" size={30} />
             </div>
         </div>
         </div>
         
+        {/* Gallery Project */}
         <div className="bg-white p-8 rounded-lg shadow-lg border-t-4 border-blue-600 flex flex-col md:flex-row-reverse items-center">
         <div className="w-full md:w-1/2 flex justify-center mb-6 md:mb-0">
-            <img src={marioImage} alt="Project 2" className="w-full max-w-xl h-auto object-cover rounded-lg shadow-lg border-4 border-blue-600" />
+            <img src={galeryImage} alt="Galery" className="w-full max-w-xl h-auto object-cover rounded-lg shadow-lg border-4 border-blue-600" />
         </div>
         <div className="w-full md:w-1/2 text-center md:text-left px-6">
-            <h3 className="text-2xl font-semibold mb-2">Project Title 2</h3>
-            <p className="mb-4">A brief description of the project goes here. Highlight key features and technologies used.</p>
+            <h3 className="text-2xl font-semibold mb-2">Galery</h3>
+            <p className="mb-4">Just a frontend mini-project that I used to study a little bit.</p>
             <div className="flex space-x-4 justify-center md:justify-start">
-            <button className="bg-blue-500 text-white px-6 py-3 rounded-full hover:bg-blue-600 transition duration-300">See Live</button>
             <button className="bg-gray-500 text-white px-6 py-3 rounded-full hover:bg-gray-600 transition duration-300">Source Code</button>
+            </div>
+            <div className="flex space-x-4 mt-4">
+              <FaSass className="text-blue-700" size={30} />
+              <FaHtml5 className="text-blue-700" size={30} />
             </div>
         </div>
         </div>
     </div>
     </div>
-      {/* Contact Section */}
-      <div className="bg-gradient-to-r from-gray-800 via-gray-700 to-gray-900 text-white min-h-[40vh] text-center px-4 py-8 flex flex-col justify-center items-center">
-        <h2 className="text-4xl font-bold mb-4 text-red-400">Contact</h2>
-        <p className="mb-6">Get in touch to discuss your next big project.</p>
-        <button className="bg-red-500 text-white px-6 py-4 rounded-full hover:bg-red-600 transition duration-300">Call to Action</button>
+{/* Contact Section */}
+<div className="bg-gradient-to-r from-gray-800 via-gray-700 to-gray-900 text-white min-h-[60vh] text-center px-8 py-12 flex flex-col justify-center items-center space-y-6">
+  <h2 className="text-4xl font-bold mb-4 text-red-400">Contact</h2>
+  <p className="text-lg mb-6">Let's work together on your next big project. Whether you have a question or want to get started, I'd love to hear from you!</p>
+  
+    {/* Contact Form */}
+    <form className="w-full max-w-lg">
+      <div className="flex flex-col mb-4">
+        <label className="mb-2 text-sm font-semibold text-left" htmlFor="name">Name</label>
+        <input
+          className="bg-gray-800 text-white rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-red-500"
+          type="text"
+          id="name"
+          placeholder="Your Name"
+        />
       </div>
+      <div className="flex flex-col mb-4">
+        <label className="mb-2 text-sm font-semibold text-left" htmlFor="email">Email</label>
+        <input
+          className="bg-gray-800 text-white rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-red-500"
+          type="email"
+          id="email"
+          placeholder="Your Email"
+        />
+      </div>
+      <div className="flex flex-col mb-4">
+        <label className="mb-2 text-sm font-semibold text-left" htmlFor="message">Message</label>
+        <textarea
+          className="bg-gray-800 text-white rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-red-500"
+          id="message"
+          placeholder="Write your message here..."
+          rows="4"
+        ></textarea>
+      </div>
+      <button className="bg-red-500 text-white px-6 py-4 rounded-full hover:bg-red-600 transition duration-300 w-full">Send Message</button>
+    </form>
+
+    {/* Additional Contact Info */}
+    <div className="mt-8">
+      <p className="text-lg">Or reach me directly at:</p>
+      <a href="mailto:guibio24@proton.me" className="text-red-400 underline">guibio24@proton.me</a>
+      <p className="mt-2">Phone: <a href="tel:+1234567890" className="text-red-400 underline">+55 11 9 1337-1300</a></p>
+    </div>
+  </div>
 
       {/* Footer Section */}
       <footer className="bg-gray-900 text-white py-8 text-center">
         <div className="flex justify-center space-x-4 mb-4">
-          <a href="#" className="text-white hover:text-gray-400"><i className="fab fa-github fa-2x"></i></a>
-          <a href="#" className="text-white hover:text-gray-400"><i className="fab fa-linkedin fa-2x"></i></a>
-          <a href="#" className="text-white hover:text-gray-400"><i className="fab fa-twitter fa-2x"></i></a>
+          <a href="#" className="text-white hover:text-gray-400"><i className="fab fa-2x"><FaGithub></FaGithub></i></a>
+          <a href="#" className="text-white hover:text-gray-400"><i className="fab fa-linkedin fa-2x"><FaDiscord></FaDiscord></i></a>
         </div>
         <p>&copy; 2024 Guilherme. All rights reserved.</p>
       </footer>
